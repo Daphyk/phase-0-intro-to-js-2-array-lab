f33fd01bf1fd58130cfc2bc0c61e0a2426905683
Daphyk <kendid07gmail.com>
2023-09-21 19:15:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+// helpers.js
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+
+function appendCat(name) {
+   const newArray = [...cats, name];
+   return newArray;
+}
+
+function prependCat(name) {
+    const newArray = [name, ...cats];
+    return newArray;
+}
+
+function removeLastCat() {
+    const newArray = cats.slice(0, -1);
+    return newArray;
+  }
+  /*
+function removeLastCat() {
+  const newArray = cats.slice(0, -1);
+  return newArray;
+}
+*/
+function removeFirstCat(name) {
+    const newArray = cats.slice(1);
+    return newArray;
+} 
